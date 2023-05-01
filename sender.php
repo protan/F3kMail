23f726b25fa35cfa95f9b644ac97ba627a979ee5
Protan Halder <80687119+protan@users.noreply.github.com>
2023-05-01 11:54:10 +0600

    Update sender.php

--- sender.php
+++ sender.php
@@ -60,48 +60,50 @@ echo '
 
 
 <footer class="bg-dark text-center text-white">
-    <!-- Grid container -->
-    <div class="container p-4 pb-0">
-        <!-- Section: Social media -->
-        <section class="mb-4">
-            <!-- Facebook -->
-            <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/protanhalder.2/" role="button"><i
-                    class="fab fa-facebook-f"></i></a>
+<!-- Grid container -->
+<div class="container p-4 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+        <!-- Facebook -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/ItsProtanHalder/" role="button"><i
+                class="fab fa-facebook-f"></i></a>
 
-            <!-- Twitter -->
-            <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/ProtanHalder" role="button"><i
-                    class="fab fa-twitter"></i></a>
+        <!-- Twitter -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/ProtanHalder" role="button"><i
+                class="fab fa-twitter"></i></a>
 
-            <!-- Google -->
-            <a class="btn btn-outline-light btn-floating m-1" href="https://protan2206.github.io/ProtanHalder/" role="button"><i
-                    class="fab fa-google"></i></a>
+        <!-- Google -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://protan2206.github.io/ProtanHalder/" role="button"><i
+                class="fab fa-google"></i></a>
 
-            <!-- Instagram -->
-            <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/protanhalder/" role="button"><i
-                    class="fab fa-instagram"></i></a>
+        <!-- Instagram -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/protanhalder/" role="button"><i
+                class="fab fa-instagram"></i></a>
 
-            <!-- Linkedin -->
-            <a class="btn btn-outline-light btn-floating m-1" href="https://bd.linkedin.com/in/protanhalder" role="button"><i
-                    class="fab fa-linkedin-in"></i></a>
+        <!-- Linkedin -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://bd.linkedin.com/in/protanhalder" role="button"><i
+                class="fab fa-linkedin-in"></i></a>
 
-            <!-- Telegram -->
-            <a class="btn btn-outline-light btn-floating m-1" href="https://t.me/protan2206" role="button"><i
-                class="fab fa-brands fa-telegram"></i></a>
+        <!-- Telegram -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://t.me/ProtanHalder" role="button"><i
+            class="fab fa-brands fa-telegram"></i></a>
 
-            <!-- Github -->
-            <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/protan2206/"
-                role="button"><i class="fab fa-github"></i></a>
-        </section>
-        <!-- Section: Social media -->
-    </div>
-    <!-- Grid container -->
+        <!-- Github -->
+        <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/protan/"
+            role="button"><i class="fab fa-github"></i></a>
+    </section>
+    <!-- Section: Social media -->
+</div>
+<!-- Grid container -->
 
-    <!-- Copyright -->
-    <div class="rounded text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2022 Copyright:
-        <a class="text-white" href="https://www.github.com/protan2206/">PROTAN HALDER</a>
-    </div>
-    <!-- Copyright -->
+<!-- Copyright -->
+<div class="rounded text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright:
+    <a class="text-white" href="https://www.github.com/protan/">PROTAN HALDER</a>
+</div>
+<!-- Copyright -->
 </footer>
+
+
 
 ';
